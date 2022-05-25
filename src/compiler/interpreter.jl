1a9db60edb27ec3c16e6e044d9b1f0a5f857a7ff
William S. Moses <gh@wsmoses.com>
2022-05-25 18:19:48 -0400

    Fix 1.6

--- src/compiler/interpreter.jl
+++ src/compiler/interpreter.jl
@@ -64,8 +64,9 @@ if isdefined(Base.Experimental, Symbol("@overlay"))
 Core.Compiler.method_table(interp::EnzymeInterpeter, sv::InferenceState) =
     Core.Compiler.OverlayMethodTable(interp.world, interp.method_table)
 else
+using GPUCompiler: WorldOverlayMethodTable
 Core.Compiler.method_table(interp::EnzymeInterpeter, sv::InferenceState) =
-    GPUCompiler.WorldOverlayMethodTable(interp.world)
+    WorldOverlayMethodTable(interp.world)
 end
 
 function is_primitive_func(@nospecialize(TT))
